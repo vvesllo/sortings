eb247497888b5fe234a70c5a34a999ae88443fc7
vvesllo <timyr2005kadirov@gmail.com>
2024-11-27 16:23:24 +0500

    buildable commit

--- sorting.hpp
+++ sorting.hpp
@@ -6,20 +6,20 @@
 
 #include <random>
 
-bool is_sorted(int* arr, size_t size);
-void shuffle(int* arr, size_t size);
-void swap(int& a, int& b);
+extern bool is_sorted(int* arr, size_t size);
+extern void shuffle(int* arr, size_t size);
+extern void swap(int& a, int& b);
 
-void insertion_sort(int* arr, size_t size);
-void selection_sort(int* arr, size_t size);
-void comb_sort(int* arr, size_t size);
-void gnome_sort(int* arr, size_t size);
-void shaker_sort(int* arr, size_t size);
-void counting_sort(int* arr, size_t size);
-void quick_sort(int* arr, size_t size, int low, int high);
-void quick_sort_random(int* arr, size_t size, int low, int high);
-void merge_sort(int* arr, size_t size, int left, int right);
-void merge_sort_no_recurse(int* arr, size_t size, int left, int right);
-void shell_sort(int* arr, size_t size);
+extern void insertion_sort(int* arr, size_t size);
+extern void selection_sort(int* arr, size_t size);
+extern void comb_sort(int* arr, size_t size);
+extern void gnome_sort(int* arr, size_t size);
+extern void shaker_sort(int* arr, size_t size);
+extern void counting_sort(int* arr, size_t size);
+extern void quick_sort(int* arr, size_t size, int low, int high);
+extern void quick_sort_random(int* arr, size_t size, int low, int high);
+extern void merge_sort(int* arr, size_t size, int left, int right);
+extern void merge_sort_no_recurse(int* arr, size_t size, int left, int right);
+extern void shell_sort(int* arr, size_t size);
 
 #endif
