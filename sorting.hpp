dca617d64b49ed5f6307eb39da980cc6ee74fdd0
vvesllo <timyr2005kadirov@gmail.com>
2024-12-05 15:17:34 +0500

    time counter

--- sorting.hpp
+++ sorting.hpp
@@ -7,7 +7,8 @@
 #include <random>
 
 extern bool is_sorted(int* arr, size_t size);
-extern void shuffle(int* arr, size_t size);
+extern bool is_reversed(int* arr, size_t size);
+extern void shuffle(int* arr, size_t size, int seed);
 extern void swap(int& a, int& b);
 
 extern void insertion_sort(int* arr, size_t size);
@@ -16,10 +17,25 @@ extern void comb_sort(int* arr, size_t size);
 extern void gnome_sort(int* arr, size_t size);
 extern void shaker_sort(int* arr, size_t size);
 extern void counting_sort(int* arr, size_t size);
-extern void quick_sort(int* arr, size_t size, int low, int high);
-extern void quick_sort_random(int* arr, size_t size, int low, int high);
-extern void merge_sort(int* arr, size_t size, int left, int right);
-extern void merge_sort_no_recurse(int* arr, size_t size, int left, int right);
+extern void quick_sort(int* arr, size_t size, size_t low, size_t high);
+extern void quick_random_sort(int* arr, size_t size, size_t low, size_t high);
+extern void merge_sort(int* arr, size_t size, size_t left, size_t right);
+extern void merge_no_recurse_sort(int* arr, size_t size, size_t left, size_t right);
 extern void shell_sort(int* arr, size_t size);
+
+
+extern void insertion_sort_reversed(int* arr, size_t size);
+extern void selection_sort_reversed(int* arr, size_t size);
+extern void comb_sort_reversed(int* arr, size_t size);
+extern void gnome_sort_reversed(int* arr, size_t size);
+extern void shaker_sort_reversed(int* arr, size_t size);
+extern void counting_sort_reversed(int* arr, size_t size);
+extern void quick_sort_reversed(int* arr, size_t size, size_t low, size_t high);
+extern void quick_random_sort_reversed(int* arr, size_t size, size_t low, size_t high);
+extern void merge_sort_reversed(int* arr, size_t size, size_t left, size_t right);
+extern void merge_no_recurse_sort_reversed(int* arr, size_t size, size_t left, size_t right);
+extern void shell_sort_reversed(int* arr, size_t size);
+
+
 
 #endif
